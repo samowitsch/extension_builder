1ee37b18eb97b82b95992b1e8c1ab264d9af329b
Nico de Haen <mail@ndh-websolutions.de>
2019-10-22 19:45:55 +0200

    Transform vendor name for composer info
    resolves #193

--- Classes/Domain/Model/Extension.php
+++ Classes/Domain/Model/Extension.php
@@ -893,7 +893,7 @@ class Extension
     {
         $composerExtensionKey = strtolower(str_replace('_', '-', $this->extensionKey));
         $info = [
-            'name' => strtolower($this->vendorName) . '/' . $composerExtensionKey,
+            'name' => strtolower(str_replace('_', '-', GeneralUtility::camelCaseToLowerCaseUnderscored($this->vendorName))) . '/' . $composerExtensionKey,
             'type' => 'typo3-cms-extension',
             'description' => $this->description,
             'authors' => [],
@@ -911,7 +911,6 @@ class Extension
                 ]
             ],
             'replace' => [
-                strtolower($this->vendorName) . '/' . $composerExtensionKey => 'self.version',
                 'typo3-ter/' . $composerExtensionKey => 'self.version'
             ]
         ];
